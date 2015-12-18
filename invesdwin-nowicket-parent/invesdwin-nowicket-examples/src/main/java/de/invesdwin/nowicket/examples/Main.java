88f8db95372ccd663eefbab8c1b365ea7a4774a8
Daaaniel <daaaniel@protonmail.ch>
2015-12-18 16:22:05 +0100

    Registration of nowicket specific error pages

--- invesdwin-nowicket-parent/invesdwin-nowicket-examples/src/main/java/de/invesdwin/nowicket/examples/Main.java
+++ invesdwin-nowicket-parent/invesdwin-nowicket-examples/src/main/java/de/invesdwin/nowicket/examples/Main.java
@@ -1,16 +1,26 @@
 package de.invesdwin.nowicket.examples;
 
+import javax.servlet.DispatcherType;
 import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.ErrorPage;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
 import org.springframework.boot.context.embedded.ServletContextInitializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
 
 import de.invesdwin.nowicket.examples.internal.NoWicketExamplesWebApplication;
 import de.invesdwin.nowicket.examples.internal.NoWicketExamplesWicketFilter;
+import de.invesdwin.nowicket.page.error.defaultpage.DefaultAccessDeniedPage;
+import de.invesdwin.nowicket.page.error.defaultpage.DefaultInternalErrorPage;
+import de.invesdwin.nowicket.page.error.defaultpage.DefaultPageExpiredPage;
+import de.invesdwin.nowicket.page.error.defaultpage.DefaultPageNotFoundPage;
 
 @SpringBootApplication
 public class Main {
@@ -20,16 +30,29 @@ public class Main {
 	}
 
 	@Bean
-	public ServletContextInitializer wicketInitializer() {
-		return new ServletContextInitializer() {
+	public FilterRegistrationBean noWicketInitializer() {
+		FilterRegistrationBean noWicketFilter = new FilterRegistrationBean();
+		noWicketFilter.setFilter(new NoWicketExamplesWicketFilter());
+		noWicketFilter.addInitParameter("applicationClassName", NoWicketExamplesWebApplication.class.getName());
+		noWicketFilter.addInitParameter("filterMappingUrlPattern", "/*");
+		noWicketFilter.addUrlPatterns("/*");
+		noWicketFilter.setDispatcherTypes(DispatcherType.REQUEST, DispatcherType.ERROR);
+		return noWicketFilter;
+	}
+
+	@Bean
+	public EmbeddedServletContainerCustomizer noWicketCustomizer() {
+		return new EmbeddedServletContainerCustomizer() {
+
 			@Override
-			public void onStartup(ServletContext servletContext) throws ServletException {
-				FilterRegistration filter = servletContext.addFilter("wicket-filter",
-						NoWicketExamplesWicketFilter.class);
-				filter.setInitParameter("applicationClassName", NoWicketExamplesWebApplication.class.getName());
-				filter.setInitParameter("filterMappingUrlPattern", "/*");
-				filter.addMappingForUrlPatterns(null, false, "/*");
+			public void customize(ConfigurableEmbeddedServletContainer container) {
+				container.addErrorPages(new ErrorPage(HttpStatus.UNAUTHORIZED, DefaultAccessDeniedPage.MOUNT_PATH));
+				container.addErrorPages(new ErrorPage(HttpStatus.FORBIDDEN, DefaultAccessDeniedPage.MOUNT_PATH));
+				container.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, DefaultPageNotFoundPage.MOUNT_PATH));
+				container.addErrorPages(new ErrorPage(HttpStatus.GONE, DefaultPageExpiredPage.MOUNT_PATH));
+				container.addErrorPages(new ErrorPage(DefaultInternalErrorPage.MOUNT_PATH));
 			}
 		};
+
 	}
 }
