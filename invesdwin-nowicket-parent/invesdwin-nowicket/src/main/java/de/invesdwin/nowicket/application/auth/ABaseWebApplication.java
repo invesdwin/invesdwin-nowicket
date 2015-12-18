2d2c26e63b58f24fe4caf4a888e6776dbce01c4b
subes <gsubes@gmail.com>
2015-12-18 15:42:22 +0100

    javadocs

--- invesdwin-nowicket-parent/invesdwin-nowicket/src/main/java/de/invesdwin/nowicket/application/auth/ABaseWebApplication.java
+++ invesdwin-nowicket-parent/invesdwin-nowicket/src/main/java/de/invesdwin/nowicket/application/auth/ABaseWebApplication.java
@@ -13,7 +13,8 @@ import de.invesdwin.nowicket.application.IWebApplicationConfig;
 import de.invesdwin.nowicket.application.filter.init.hook.IWebApplicationInitializerHook;
 
 @NotThreadSafe
-public abstract class ABaseWebApplication extends org.apache.wicket.authroles.authentication.AuthenticatedWebApplication {
+public abstract class ABaseWebApplication
+        extends org.apache.wicket.authroles.authentication.AuthenticatedWebApplication {
 
     public static ABaseWebApplication get() {
         return (ABaseWebApplication) org.apache.wicket.authroles.authentication.AuthenticatedWebApplication.get();
@@ -48,10 +49,20 @@ public abstract class ABaseWebApplication extends org.apache.wicket.authroles.au
         return (AWebSession) super.newSession(request, response);
     }
 
+    /**
+     * Provide a string here to enable cookies to last over server restarts. This is required to have the remember-me
+     * feature work properly.
+     */
     public abstract String getSessionEncryptionKey();
 
+    /**
+     * These packages are scanned for model classes to generate markup for.
+     */
     public abstract Set<String> getClasspathBasePackages();
 
+    /**
+     * Temporary session files (like uploads) will be stored in this folder.
+     */
     public abstract File getSessionsDirectory();
 
 }
