9279a15b5a54f6aac4ded93af8f27c1ea717842a
Daaaniel <daaaniel@protonmail.ch>
2015-12-18 17:03:08 +0100

    implementatin of the MarkupGenerator and some refactoring

--- invesdwin-nowicket-parent/invesdwin-nowicket-examples/src/main/java/de/invesdwin/nowicket/examples/internal/ExampleWicketFilter.java
+++ invesdwin-nowicket-parent/invesdwin-nowicket-examples/src/main/java/de/invesdwin/nowicket/examples/internal/ExampleWicketFilter.java
@@ -3,11 +3,11 @@ package de.invesdwin.nowicket.examples.internal;
 import de.invesdwin.nowicket.application.IWebApplicationConfig;
 import de.invesdwin.nowicket.application.filter.AWicketFilter;
 
-public class NoWicketExamplesWicketFilter extends AWicketFilter {
+public class ExampleWicketFilter extends AWicketFilter {
 
 	@Override
 	protected IWebApplicationConfig newConfig() {
-		return new NoWicketExamplesWebApplicationConfig();
+		return new ExampleWebApplicationConfig();
 	}
 
 }
