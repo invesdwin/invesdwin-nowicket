95e6c3f023d91f37263f594128400c0ae36eac23
daaaniel <daaaniel@protonmail.ch>
2015-12-18 14:36:12 +0100

    example project for invesdwin-nowicket

---
+++ invesdwin-nowicket-parent/invesdwin-nowicket-examples/src/main/java/de/invesdwin/nowicket/examples/NoWicketExamplesApplication.java
@@ -0,0 +1,12 @@
+package de.invesdwin.nowicket.examples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoWicketExamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoWicketExamplesApplication.class, args);
+    }
+}
