4758b511fb1b7984d388b67e2d6d2e99d0740b37
subes <gsubes@gmail.com>
2016-03-09 23:49:50 +0100

    navigation

--- invesdwin-nowicket-parent/invesdwin-nowicket-examples/invesdwin-nowicket-examples-guide/src/main/java/de/invesdwin/nowicket/examples/guide/pages/documentation/concept/Concept.java
+++ invesdwin-nowicket-parent/invesdwin-nowicket-examples/invesdwin-nowicket-examples-guide/src/main/java/de/invesdwin/nowicket/examples/guide/pages/documentation/concept/Concept.java
@@ -2,11 +2,20 @@ package de.invesdwin.nowicket.examples.guide.pages.documentation.concept;
 
 import javax.annotation.concurrent.NotThreadSafe;
 
+import de.invesdwin.nowicket.examples.guide.pages.home.Home;
 import de.invesdwin.nowicket.generated.markup.annotation.GeneratedMarkup;
 import de.invesdwin.util.bean.AValueObject;
 
 @NotThreadSafe
 @GeneratedMarkup
 public class Concept extends AValueObject {
+
+    public Home goBackToIntroduction() {
+        return new Home();
+    }
+
+    public Object readNextChapter() {
+        return null;
+    }
 
 }
