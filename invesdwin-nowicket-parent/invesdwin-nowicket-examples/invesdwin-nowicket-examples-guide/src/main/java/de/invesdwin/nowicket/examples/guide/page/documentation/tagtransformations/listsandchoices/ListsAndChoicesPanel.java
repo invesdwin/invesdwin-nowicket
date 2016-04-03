0e5175c1cbf94d437ffd969aeac15337009b2619
subes <gsubes@gmail.com>
2016-04-04 00:17:03 +0200

    compile fix

--- invesdwin-nowicket-parent/invesdwin-nowicket-examples/invesdwin-nowicket-examples-guide/src/main/java/de/invesdwin/nowicket/examples/guide/page/documentation/tagtransformations/listsandchoices/ListsAndChoicesPanel.java
+++ invesdwin-nowicket-parent/invesdwin-nowicket-examples/invesdwin-nowicket-examples-guide/src/main/java/de/invesdwin/nowicket/examples/guide/page/documentation/tagtransformations/listsandchoices/ListsAndChoicesPanel.java
@@ -6,7 +6,6 @@ import org.apache.wicket.Component;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.IModel;
 
-import de.invesdwin.nowicket.examples.guide.page.documentation.tagtransformations.lists.ListsConstants;
 import de.invesdwin.nowicket.generated.binding.GeneratedBinding;
 import de.invesdwin.nowicket.generated.binding.processor.element.SelectHtmlElement;
 import de.invesdwin.nowicket.generated.binding.processor.visitor.builder.BindingInterceptor;
@@ -20,7 +19,7 @@ public class ListsAndChoicesPanel extends Panel {
         new GeneratedBinding(this).withBindingInterceptor(new BindingInterceptor() {
             @Override
             public Component createSelect(final SelectHtmlElement e) {
-                if (ListsConstants.asMultiselectPalette.equals(e.getWicketId())) {
+                if (ListsAndChoicesConstants.asMultiselectPalette.equals(e.getWicketId())) {
                     return new ModelPalette(e);
                 }
                 return super.createSelect(e);
