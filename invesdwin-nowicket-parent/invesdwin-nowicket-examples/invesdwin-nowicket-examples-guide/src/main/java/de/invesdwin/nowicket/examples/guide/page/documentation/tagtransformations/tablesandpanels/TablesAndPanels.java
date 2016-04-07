fe042574cba61bcf369604f7a88ab77fa9bb9beb
subes <gsubes@gmail.com>
2016-04-07 02:05:35 +0200

    tabbed with different panels

--- invesdwin-nowicket-parent/invesdwin-nowicket-examples/invesdwin-nowicket-examples-guide/src/main/java/de/invesdwin/nowicket/examples/guide/page/documentation/tagtransformations/tablesandpanels/TablesAndPanels.java
+++ invesdwin-nowicket-parent/invesdwin-nowicket-examples/invesdwin-nowicket-examples-guide/src/main/java/de/invesdwin/nowicket/examples/guide/page/documentation/tagtransformations/tablesandpanels/TablesAndPanels.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.annotation.concurrent.NotThreadSafe;
 
 import de.invesdwin.nowicket.examples.guide.page.documentation.tagtransformations.tablesandchoices.row.ExtendedTableRow;
+import de.invesdwin.nowicket.examples.guide.page.documentation.userinterfaceflow.fifthcar.details.tabs.info.CarInfo;
 import de.invesdwin.nowicket.generated.binding.annotation.Eager;
 import de.invesdwin.nowicket.generated.markup.annotation.GeneratedMarkup;
 import de.invesdwin.util.bean.AValueObject;
@@ -25,30 +26,37 @@ public class TablesAndPanels extends AValueObject {
     };
 
     private final List<ExtendedTableRow> defaultTable = Objects.deepClone(choices);
-    private final List<ExtendedTableRow> asUnorderedList = Objects.deepClone(choices);
-    private final List<ExtendedTableRow> asCollapsible = Objects.deepClone(choices);
-    private final List<ExtendedTableRow> asAccordion = Objects.deepClone(choices);
-    private final List<?> asTabbed = Objects.deepClone(choices);
+    private final List<Object> asUnorderedList = (List) Objects.deepClone(choices);
+    private final List<Object> asCollapsible = (List) Objects.deepClone(choices);
+    private final List<Object> asAccordion = (List) Objects.deepClone(choices);
+    private final List<Object> asTabbed = (List) Objects.deepClone(choices);
+
+    public TablesAndPanels() {
+        /**
+         * We don't require static typing for these panel lists, thus you can even mix different types into one list
+         */
+        asUnorderedList.add(new CarInfo("4-1", "4-2"));
+        asCollapsible.add(new CarInfo("4-1", "4-2"));
+        asAccordion.add(new CarInfo("4-1", "4-2"));
+        asTabbed.add(new CarInfo("4-1", "4-2"));
+    }
 
     public List<ExtendedTableRow> getDefaultTable() {
         return defaultTable;
     }
 
-    public List<ExtendedTableRow> getAsCollapsible() {
+    public List<Object> getAsCollapsible() {
         return asCollapsible;
     }
 
-    public List<ExtendedTableRow> getAsAccordion() {
+    public List<Object> getAsAccordion() {
         return asAccordion;
     }
 
-    public List<ExtendedTableRow> getAsUnorderedList() {
+    public List<Object> getAsUnorderedList() {
         return asUnorderedList;
     }
 
-    /**
-     * We don't require static typing for tabbed panels, thus you can even mix different types into one list
-     */
     public List<?> getAsTabbed() {
         return asTabbed;
     }
