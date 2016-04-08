afad96392fd022b9a22e0c6fabcd8a013b27479d
subes <gsubes@gmail.com>
2016-04-08 20:00:37 +0200

    fix copy/paste in modals

--- invesdwin-nowicket-parent/invesdwin-nowicket/src/main/java/de/invesdwin/nowicket/component/header/allowCopyPasteOnDisabledInputs.js
+++ invesdwin-nowicket-parent/invesdwin-nowicket/src/main/java/de/invesdwin/nowicket/component/header/allowCopyPasteOnDisabledInputs.js
@@ -1,66 +1,85 @@
 function allowCopyPasteOnDisabledInputs() {
+	if (typeof allowCopyPasteOnDisabledInputsRegistered === 'undefined') {
+		window.allowCopyPasteOnDisabledInputsRegistered = true;
 
-	function replaceWithReadonlyInput(tag, replaceWithTag, value) {
-		var fixedTag = getAlreadyFixedTagFor(tag);
-		if (fixedTag.length) {
-			// update existing fix
-			fixedTag.attr("type", tag.attr("type"));
-			fixedTag.attr("class", tag.attr("class"));
-			fixedTag.attr("value", value);
-			fixedTag.attr("title", tag.attr("title"));
-			fixedTag.attr("rows", tag.attr("rows"));
-			fixedTag.attr("id", tag.attr("id")
-					+ "_allowCopyPasteOnDisabledInputs");
+		function triggerAllowCopyPasteOnDisabledInputs() {
 
-			return;
-		} else {
-			// replace tag with readonly input and make tag invisible
-			tag.attr("style", "display:none");
-			var input = $(replaceWithTag);
+			function replaceWithReadonlyInput(tag, replaceWithTag, value) {
+				if (tag.attr("id").indexOf("_allowCopyPasteOnDisabledInputs") > -1) {
+					return;
+				}
 
-			input.attr("data-allowCopyPasteOnDisabledInputs", tag.attr("id"));
-			input.attr("readonly", "readonly");
-
-			// copy important attributes
-			input.attr("type", tag.attr("type"));
-			input.attr("class", tag.attr("class"));
-			input.attr("value", value);
-			input.attr("title", tag.attr("title"));
-			input.attr("rows", tag.attr("rows"));
-			input
-					.attr("id", tag.attr("id")
+				var fixedTag = getAlreadyFixedTagFor(tag);
+				if (fixedTag.length) {
+					// update existing fix
+					fixedTag.attr("type", tag.attr("type"));
+					fixedTag.attr("class", tag.attr("class"));
+					fixedTag.attr("value", value);
+					fixedTag.attr("title", tag.attr("title"));
+					fixedTag.attr("rows", tag.attr("rows"));
+					fixedTag.attr("id", tag.attr("id")
 							+ "_allowCopyPasteOnDisabledInputs");
 
-			return input;
-		}
-	}
+					return;
+				} else {
+					// replace tag with readonly input and make tag invisible
+					var input = $(replaceWithTag);
 
-	function getAlreadyFixedTagFor(tag) {
-		return $('[data-allowCopyPasteOnDisabledInputs=' + tag.attr("id") + ']')
-	}
+					input.attr("data-allowCopyPasteOnDisabledInputs", tag
+							.attr("id"));
+					input.attr("readonly", "readonly");
 
-	$('textarea:disabled:visible').after(function(e) {
-		var tag = $(this);
-		var value = tag.val();
-		var input = replaceWithReadonlyInput(tag, "<textarea>", value);
-		return input;
-	});
-	$('select:disabled:visible').filter(function() {
-		var size = $(this).attr("size");
-		return !(size > 1);
-	}).after(function(e) {
-		var tag = $(this);
-		var value = tag.find("option:selected").text();
-		var input = replaceWithReadonlyInput(tag, "<input>", value);
-		return input;
-	});
-	$(
-			'input:disabled:not([type=radio]):not([type=checkbox]):not([type=file]):visible')
-			.after(function(e) {
+					// copy important attributes
+					input.attr("type", tag.attr("type"));
+					input.attr("class", tag.attr("class"));
+					input.attr("value", value);
+					input.attr("title", tag.attr("title"));
+					input.attr("rows", tag.attr("rows"));
+					input.attr("id", tag.attr("id")
+							+ "_allowCopyPasteOnDisabledInputs");
+
+					tag.attr("style", "display:none");
+
+					return input;
+				}
+			}
+
+			function getAlreadyFixedTagFor(tag) {
+				return $('[data-allowCopyPasteOnDisabledInputs='
+						+ tag.attr("id") + ']')
+			}
+
+			$('textarea:disabled:visible').after(function(e) {
 				var tag = $(this);
 				var value = tag.val();
+				var input = replaceWithReadonlyInput(tag, "<textarea>", value);
+				return input;
+			});
+			$('select:disabled:visible').filter(function() {
+				var size = $(this).attr("size");
+				return !(size > 1);
+			}).after(function(e) {
+				var tag = $(this);
+				var value = tag.find("option:selected").text();
 				var input = replaceWithReadonlyInput(tag, "<input>", value);
 				return input;
 			});
+			$(
+					'input:disabled:not([type=radio]):not([type=checkbox]):not([type=file]):visible')
+					.after(
+							function(e) {
+								var tag = $(this);
+								var value = tag.val();
+								var input = replaceWithReadonlyInput(tag,
+										"<input>", value);
+								return input;
+							});
 
+		}
+
+		triggerAllowCopyPasteOnDisabledInputs();
+		Wicket.Event.add(window, 'shown.bs.modal', function(e) {
+			triggerAllowCopyPasteOnDisabledInputs();
+		});
+	}
 }
