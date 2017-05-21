8259a520838a462f37cb9dfa0d538ef990fdb305
subes <gsubes@gmail.com>
2017-05-21 21:07:35 +0200

    checkstyle fix

--- invesdwin-nowicket-parent/invesdwin-nowicket-examples/invesdwin-nowicket-examples-guide/src/main/java/de/invesdwin/nowicket/examples/guide/page/wicket/fileupload/FileUploadPage.java
+++ invesdwin-nowicket-parent/invesdwin-nowicket-examples/invesdwin-nowicket-examples-guide/src/main/java/de/invesdwin/nowicket/examples/guide/page/wicket/fileupload/FileUploadPage.java
@@ -6,7 +6,6 @@ import org.apache.wicket.Component;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
-import org.apache.wicket.util.lang.Bytes;
 import org.wicketstuff.annotation.mount.MountPath;
 
 import de.invesdwin.nowicket.examples.guide.page.AExampleWebPage;
@@ -30,9 +29,9 @@ public class FileUploadPage extends AExampleWebPage {
             public Component createForm(final FormHtmlElement e) {
                 final Form<?> form = (Form<?>) new DefaultBindingBuilder().createForm(e);
                 // Set maximum size per file to 90K for demo purposes
-                form.setFileMaxSize(Bytes.kilobytes(90));
+                form.setFileMaxSize(org.apache.wicket.util.lang.Bytes.kilobytes(90));
                 // Set maximum size to 100K for demo purposes
-                form.setMaxSize(Bytes.kilobytes(100));
+                form.setMaxSize(org.apache.wicket.util.lang.Bytes.kilobytes(100));
                 return form;
 
             }
