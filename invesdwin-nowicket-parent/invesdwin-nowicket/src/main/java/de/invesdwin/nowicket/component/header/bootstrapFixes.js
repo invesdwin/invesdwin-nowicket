4222a165063803742849769f6511995239cce24a
Edwin Stang <gsubes@gmail.com>
2022-09-07 01:14:15 +0200

    disable transitions in general

--- invesdwin-nowicket-parent/invesdwin-nowicket/src/main/java/de/invesdwin/nowicket/component/header/bootstrapFixes.js
+++ invesdwin-nowicket-parent/invesdwin-nowicket/src/main/java/de/invesdwin/nowicket/component/header/bootstrapFixes.js
@@ -1,5 +1,4 @@
 $(function() { 
 	//https://stackoverflow.com/questions/13119912/disable-bootstraps-collapse-open-close-animation
-	$.fn.collapse.Constructor.TRANSITION_DURATION = 0; 
-	//$.support.transition = false;
+	$.support.transition = false;
 })
