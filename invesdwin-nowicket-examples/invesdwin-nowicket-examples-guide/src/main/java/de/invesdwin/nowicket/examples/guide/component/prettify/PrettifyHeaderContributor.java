32ff95aad8bbf1d1bb771b2002f4cc946b444342
Edwin Stang <gsubes@gmail.com>
2022-09-09 19:54:36 +0200

    fix prettyprinted background

--- invesdwin-nowicket-examples/invesdwin-nowicket-examples-guide/src/main/java/de/invesdwin/nowicket/examples/guide/component/prettify/PrettifyHeaderContributor.java
+++ invesdwin-nowicket-examples/invesdwin-nowicket-examples-guide/src/main/java/de/invesdwin/nowicket/examples/guide/component/prettify/PrettifyHeaderContributor.java
@@ -2,6 +2,7 @@ package de.invesdwin.nowicket.examples.guide.component.prettify;
 
 import javax.annotation.concurrent.Immutable;
 
+import org.apache.wicket.markup.head.CssHeaderItem;
 import org.apache.wicket.markup.head.IHeaderResponse;
 import org.apache.wicket.markup.html.IHeaderContributor;
 
@@ -16,6 +17,8 @@ public final class PrettifyHeaderContributor implements IHeaderContributor {
     public void renderHead(final IHeaderResponse response) {
         PrettifyJsReference.INSTANCE.renderHead(response);
         PrettifyCssReference.INSTANCE.renderHead(response);
+        response.render(
+                CssHeaderItem.forCSS(".prettyprinted { background-color: #f5f5f5 }", "prettyPrintedBackground"));
     }
 
 }
