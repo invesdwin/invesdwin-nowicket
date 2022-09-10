27869c4a2e0aa3ade59a7003693ba525c8d504d6
Edwin Stang <gsubes@gmail.com>
2022-09-10 18:20:05 +0200

    fix remove column invoker

--- invesdwin-nowicket-parent/invesdwin-nowicket/src/main/java/de/invesdwin/nowicket/generated/binding/processor/element/TableRemoveFromButtonColumnHtmlElement.java
+++ invesdwin-nowicket-parent/invesdwin-nowicket/src/main/java/de/invesdwin/nowicket/generated/binding/processor/element/TableRemoveFromButtonColumnHtmlElement.java
@@ -2,7 +2,10 @@ package de.invesdwin.nowicket.generated.binding.processor.element;
 
 import javax.annotation.concurrent.NotThreadSafe;
 
+import org.apache.wicket.model.IModel;
+
 import de.invesdwin.nowicket.generated.binding.processor.context.HtmlContext;
+import de.invesdwin.nowicket.generated.binding.processor.visitor.builder.component.button.callback.ISubmitButtonCallback;
 import de.invesdwin.nowicket.generated.markup.processor.element.TableRemoveFromButtonColumnModelElement;
 
 @NotThreadSafe
@@ -21,6 +24,14 @@ public class TableRemoveFromButtonColumnHtmlElement extends TableSubmitButtonCol
     @Override
     protected boolean isRowContainer() {
         return false;
+    }
+
+    @Override
+    public ISubmitButtonCallback getButtonCallback(final IModel<Object> targetObjectModel) {
+        return getContext().getSubmitButtonCallbackFactory()
+                .createSubmitButtonCallback(this, getTableObjectModel(),
+                        () -> new Object[] { targetObjectModel.getObject() },
+                        getModelElement().getBeanPathElement().getInvoker());
     }
 
 }
