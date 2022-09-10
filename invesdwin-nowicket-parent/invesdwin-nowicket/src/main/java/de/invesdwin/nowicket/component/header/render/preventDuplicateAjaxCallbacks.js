33319a125d5094a418c03c3eba56119fee1082c8
Edwin Stang <gsubes@gmail.com>
2022-09-10 18:38:05 +0200

    fix ajax timer

--- invesdwin-nowicket-parent/invesdwin-nowicket/src/main/java/de/invesdwin/nowicket/component/header/render/preventDuplicateAjaxCallbacks.js
+++ invesdwin-nowicket-parent/invesdwin-nowicket/src/main/java/de/invesdwin/nowicket/component/header/render/preventDuplicateAjaxCallbacks.js
@@ -1,15 +1,20 @@
 if (Wicket.Ajax.original_ajax == null) {
 	Wicket.Ajax.original_ajax = Wicket.Ajax.ajax;
-	
+
 	Wicket.Ajax.ajax = function(attrs) {
-		const element = document.getElementById(attrs.c);
-		if(element){
-			const attrsStr = JSON.stringify(attrs);
-			element.wicketAjaxUrls = element.wicketAjaxUrls || [];
-			if(!element.wicketAjaxUrls.includes(attrsStr)){
-				element.wicketAjaxUrls.push(attrsStr);
-				Wicket.Ajax.original_ajax(attrs);
+		if (attrs.c) {
+			const element = document.getElementById(attrs.c);
+			if (element) {
+				const attrsStr = JSON.stringify(attrs);
+				element.wicketAjaxUrls = element.wicketAjaxUrls || [];
+				if (!element.wicketAjaxUrls.includes(attrsStr)) {
+					element.wicketAjaxUrls.push(attrsStr);
+					Wicket.Ajax.original_ajax(attrs);
+				}
 			}
+		} else {
+			// ajax timer
+			Wicket.Ajax.original_ajax(attrs);
 		}
 	}
 }
