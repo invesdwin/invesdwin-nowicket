37779945eb48b930f2f3f251d11b71a83f62f03b
matztejankow <m.jankow@1-2-3-invest.de>
2022-10-18 07:10:17 +0200

    whitespace commit. check if jenkins for nowicket and after that invesdwin-trading starts automatically.

--- invesdwin-nowicket-parent/invesdwin-nowicket/src/main/java/de/invesdwin/nowicket/NoWicketProperties.java
+++ invesdwin-nowicket-parent/invesdwin-nowicket/src/main/java/de/invesdwin/nowicket/NoWicketProperties.java
@@ -11,7 +11,7 @@ public final class NoWicketProperties {
     public static final int DEFAULT_TABLE_ROWS_PER_PAGE = 20;
     public static final Duration DEFAULT_STATUS_MESSAGE_DURATION = new Duration(10, FTimeUnit.SECONDS);
     /**
-     * This is also the default max rows from ListMultipleChoice
+     * This is also the default max rows from ListMultipleChoice 
      */
     public static final int DEFAULT_SELECT_MAX_ROWS = 8;
 
