7cf55b21544b5fdfebc662973a6d0a98e5d2f8a0
Edwin Stang <gsubes@gmail.com>
2023-12-14 12:12:03 +0100

    fix sorting race condition https://github.com/invesdwin/orbox/issues/85

--- invesdwin-nowicket-parent/invesdwin-nowicket/src/main/java/de/invesdwin/nowicket/generated/binding/processor/visitor/builder/component/table/ModelSortableDataProvider.java
+++ invesdwin-nowicket-parent/invesdwin-nowicket/src/main/java/de/invesdwin/nowicket/generated/binding/processor/visitor/builder/component/table/ModelSortableDataProvider.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import javax.annotation.concurrent.NotThreadSafe;
 
+import org.apache.wicket.extensions.markup.html.repeater.util.SortParam;
 import org.apache.wicket.extensions.markup.html.repeater.util.SortableDataProvider;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
@@ -14,6 +15,8 @@ import de.invesdwin.nowicket.generated.binding.processor.element.TableHtmlElemen
 import de.invesdwin.nowicket.generated.binding.processor.visitor.builder.model.BeanPathModel;
 import de.invesdwin.util.lang.comparator.ACriteriaComparator;
 import de.invesdwin.util.lang.comparator.IComparator;
+import de.invesdwin.util.lang.comparator.ISortAlgorithm;
+import de.invesdwin.util.lang.comparator.SortAlgorithm;
 import de.invesdwin.util.math.Integers;
 
 /**
@@ -21,21 +24,6 @@ import de.invesdwin.util.math.Integers;
  */
 @NotThreadSafe
 public class ModelSortableDataProvider extends SortableDataProvider<Object, String> {
-
-    private final IComparator<Object> comparator = new ACriteriaComparator<Object>() {
-        @Override
-        public Comparable<?> getCompareCriteriaNotNullSafe(final Object e) {
-            final IModel<Object> eModel = new IModel<Object>() {
-                @Override
-                public Object getObject() {
-                    return e;
-                }
-            };
-            final IModel<Comparable<Object>> model = new BeanPathModel<Comparable<Object>>(eModel,
-                    getSort().getProperty());
-            return model.getObject();
-        }
-    };
 
     private final TableHtmlElement element;
 
@@ -45,10 +33,11 @@ public class ModelSortableDataProvider extends SortableDataProvider<Object, Stri
 
     @Override
     public Iterator<? extends Object> iterator(final long first, final long count) {
-        final List<Object> rows = element.getChoiceModel().getObject();
+        final List<Object> rows = newRows();
 
-        if (getSort() != null) {
-            comparator.asAscending(getSort().isAscending()).sort(rows);
+        final SortParam<String> sort = getSort();
+        if (sort != null) {
+            sort(rows, sort);
         }
 
         long toIndex = first + count;
@@ -58,9 +47,37 @@ public class ModelSortableDataProvider extends SortableDataProvider<Object, Stri
         return rows.subList(Integers.checkedCast(first), Integers.checkedCast(toIndex)).listIterator();
     }
 
+    protected void sort(final List<Object> rows, final SortParam<String> sort) {
+        final String sortProperty = sort.getProperty();
+        final IComparator<Object> comparator = new ACriteriaComparator<Object>() {
+            @Override
+            public Comparable<?> getCompareCriteriaNotNullSafe(final Object e) {
+                final IModel<Object> eModel = new IModel<Object>() {
+                    @Override
+                    public Object getObject() {
+                        return e;
+                    }
+                };
+                final IModel<Comparable<Object>> model = new BeanPathModel<Comparable<Object>>(eModel, sortProperty);
+                return model.getObject();
+            }
+
+            @Override
+            public ISortAlgorithm getSortAlgorithm() {
+                //a race condition could make the comparison unstable
+                return SortAlgorithm.TIMSORT_FALLBACK_BUBBLESORT;
+            }
+        };
+        comparator.asAscending(sort.isAscending()).sort(rows);
+    }
+
+    protected List<Object> newRows() {
+        return element.getChoiceModel().getObject();
+    }
+
     @Override
     public long size() {
-        final List<Object> rows = element.getChoiceModel().getObject();
+        final List<Object> rows = newRows();
         return rows.size();
     }
 
